fc1892489ddc0ac28e9d2ea733e8d75818bb79f7
RanshikaMihiran <ranshika20@gmail.com>
2023-06-21 21:35:17 +0530

    Edit utils

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -64,15 +65,21 @@ router.get("/:id", async (req,res)=>{
 
 //GET ALL
 
-router.get("/", async (req,res)=>{
- 
+router.get("/", async (req,res,next)=>{
+
+    const failed = true;
+    
+    
+
     try{
        const hotels = await Hotel.find();
         res.status(200).json(hotels);
     
     }catch(err){
-        res.status(500).json(err);
+        next(err);
     }
     });
 
 export default router
+
+
