83d8934d75567b6ac87482ab13ad6d90bfebd311
Lakshika Madhuranga <lakshikamadhuranga21@gmail.com>
2023-06-24 11:21:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,49 +7,49 @@ import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import cookieParser from "cookie-parser";
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 
-const connect  = async ()=>{
- 
+// Connect to MongoDB
+const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO);
-        console.log("Connected to mongoDB.")
+        console.log("Connected to MongoDB.");
     } catch (error) {
-      throw error;
+        throw error;
     }
 };
 
-mongoose.connection.on("disconnected", ()=>{
-    console.log("mongoDB disconnected")
-})
+// Event listener for MongoDB disconnection
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+});
 
-// middleware
-app.use(cookieParser())
+// Middleware
+app.use(cookieParser());
 app.use(express.json());
 
-app.use("/api/auth",authRoute);
-app.use("/api/users",usersRoute);
-app.use("/api/hotels",hotelsRoute);
-app.use("/api/rooms",roomsRoute);
+// Routes
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
-app.use((err,req,res,next)=>{
+// Error handling middleware
+app.use((err, req, res, next) => {
     const errorStatus = err.status || 500;
     const errorMessage = err.message || "Something went wrong!";
     return res.status(errorStatus).json({
         success: false,
         status: errorStatus,
-        message : errorMessage,
-        stack : err.stack,
-
+        message: errorMessage,
+        stack: err.stack,
     });
-    
 });
 
-
-app.listen(8800,()=>{
-    connect()
-    console.log("Connected to Backend..");
+// Start the server
+const PORT = 8800;
+app.listen(PORT, () => {
+    connect();
+    console.log("Connected to Backend.");
 });
-
-
