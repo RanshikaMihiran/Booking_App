33b0ce78270bcbb7e46a32cb89ded8f818d16c00
RanshikaMihiran <89793493+RanshikaMihiran@users.noreply.github.com>
2023-07-05 01:02:41 +0530

    edit featured

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
@@ -20,12 +20,14 @@ router.delete("/:id", verifyAdmin,deleteHotel);
 
 //GET
 
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 
 //GET ALL
 
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotel);
 
 export default router
 
